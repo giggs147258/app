64a7b963228934df497d1ab5ef17b9ff18ea305b
RTP-10305 <giggs147258@gmail.com>
2018-04-18 16:36:04 +0700

    color

--- app/src/main/java/th/rmutsv/co/th/myapplication/MainActivity.java
+++ app/src/main/java/th/rmutsv/co/th/myapplication/MainActivity.java
@@ -3,11 +3,20 @@ package th.rmutsv.co.th.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import th.rmutsv.co.th.myapplication.fragment.Mainfragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            Mainfragment mainfragment = new Mainfragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment,mainfragment)
+                    .commit();
+        }
     }
 }
---
+++ app/src/main/java/th/rmutsv/co/th/myapplication/fragment/Mainfragment.java
@@ -0,0 +1,23 @@
+package th.rmutsv.co.th.myapplication.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import th.rmutsv.co.th.myapplication.R;
+
+/**
+ * Created by RTP-10305 on 18/4/2561.
+ */
+
+public class Mainfragment extends android.support.v4.app.Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+        return view;
+    }
+}
